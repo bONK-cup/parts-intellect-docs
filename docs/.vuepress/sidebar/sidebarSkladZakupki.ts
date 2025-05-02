c5cd78f0de98e01b79eae58239a642f95526918b
bONK-cup <kokosik260302@gmail.com>
2025-05-02 20:22:14 +0300

    fix some new pages

--- docs/.vuepress/sidebar/sidebarSkladZakupki.ts
+++ docs/.vuepress/sidebar/sidebarSkladZakupki.ts
@@ -56,5 +56,59 @@ export const sidebarSkladZakupki = [
   {
     text: 'Заказы поставщикам',
     link: '/specification/sklad_i_zakupki/zakazy_postavschikam.md'
-  }  
+  },
+  {
+    text: 'Отказы поставщиков',
+    link: '/specification/sklad_i_zakupki/otkazy_postavschikov.md'
+  },
+  {
+    text: 'Приходные накладные',
+    link: '/specification/sklad_i_zakupki/prihodnye_nakladnye.md'
+  },
+  {
+    text: 'Корректировки приходных накладных',
+    link: '/specification/sklad_i_zakupki/korrektirovki_prihodnyh_nakladnyh.md'
+  },
+  {
+    text: 'Возвраты поставщикам',
+    link: '/specification/sklad_i_zakupki/vozvraty_postavschikam.md'
+  },  
+  {
+    text: 'Списания товаров',
+    link: '/specification/sklad_i_zakupki/spisanija_tovarov.md'
+  },
+  {
+    text: 'Наши заказы на склад',
+    link: '/specification/sklad_i_zakupki/nashi_zakazy_na_sklad.md'
+  },
+  {
+    text: 'Отказы по нашим заказам',
+    link: '/specification/sklad_i_zakupki/otkazy_po_nashim_zakazam.md'
+  },
+  {
+    text: 'Задания на склад',
+    collapsed: true,
+    items: [
+      {
+        text: 'Инвентаризация',
+        link: '/specification/sklad_i_zakupki/zadanija_na_sklad/inventarizatsija.md'
+      },
+      {
+        text: 'Отгрузка товара',
+        link: '/specification/sklad_i_zakupki/zadanija_na_sklad/otgruzka_tovara.md'
+      },
+      {
+        text: 'Разбор товара',
+        link: '/specification/sklad_i_zakupki/zadanija_na_sklad/razbor_tovara.md'
+      }
+    ]
+  },
+  {
+    text: 'Перемещения товаров',
+    link: '/specification/sklad_i_zakupki/peremeschenija_tovarov.md'
+  },
+  {
+    text: 'Автовозврат поставщику',
+    link: '/specification/sklad_i_zakupki/avtovozvrat_postavschiku.md'
+  }
 ]
