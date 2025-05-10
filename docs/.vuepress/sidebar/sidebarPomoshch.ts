178006413f635fc9cbbde41e14090c14cbba04ca
bONK-cup <kokosik260302@gmail.com>
2025-05-10 10:26:41 +0300

    fix new pages in spec section

---
+++ docs/.vuepress/sidebar/sidebarPomoshch.ts
@@ -0,0 +1,48 @@
+export const sidebarPomoshch = [
+  {
+    text: 'Основные элементы',
+    link: '/specification/pomoshch/README.md'
+  },
+  {
+    text: 'Мастер первичной настройки',
+    collapsed: true,
+    items: [
+      {
+        text: 'Наши фирмы',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/nashi_firmy.md'
+      },
+            {
+        text: 'Торговые точки и склады',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/torgovye_tochki_i_sklady.md'
+      },
+      {
+        text: 'Счета и кассы',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/scheta_i_kassy.md'
+      },
+      {
+        text: 'Контрагенты',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/kontragenty.md'
+      },      
+      {
+        text: 'Прайс-лист наличия',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/prajs-list_nalichiya.md'
+      },
+      {
+        text: 'Ценообразование наличия',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/tsenoobrazovanie_nalichiya.md'
+      },
+      {
+        text: 'Веб-поставщики',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/veb-postavshchiki.md'
+      },
+      {
+        text: 'Почта',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/pochta.md'
+      },
+      {
+        text: 'Что дальше',
+        link: '/specification/pomoshch/master_pervichnoj_nastrojki/chto_dalshe.md'
+      },
+    ]
+  }  
+]
